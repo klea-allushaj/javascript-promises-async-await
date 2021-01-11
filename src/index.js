904a0634329c7bdb59c1558e635942d52d51e041
klea-allushaj <61850991+klea-allushaj@users.noreply.github.com>
2021-01-11 13:43:25 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-const movies=require(./movies.json);
-export function fetchMovies(){
+import { export1 } from "module-name";
+const movies=require(./data/movies.json);
 
-}
+export function fetchMovies(){
 const resolveFunction = () => movies;
+return fetchWithTimeout(1000).then(resolveFunction);
+}
+
+const moviePromise = fetchMovies();
+moviePromise.then(results=>{
+  console.log(results);
+});
