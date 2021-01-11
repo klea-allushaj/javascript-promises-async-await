4b0402b22cfc88cf7c558d8066a9ca20d02d6f29
klea-allushaj <61850991+klea-allushaj@users.noreply.github.com>
2021-01-11 14:43:06 +0000

    Update services.js

--- src/services.js
+++ src/services.js
@@ -20,7 +20,19 @@ export function fetchBooks(){
 
 export async function asyncFetchMovies(){
   try{
-  const response=await fetch("./data/mpvies.json");
+  const response=await fetch("./data/movies.json");
+  const results=await response.json();
+  return results;
+
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+export async function asyncFetchBooks(){
+  try{
+  const response=await fetch("./data/books.json");
   const results=await response.json();
   return results;
 
